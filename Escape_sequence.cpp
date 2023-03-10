30dbb897b7411e90c79b0a990568f8d06562d0d7
ShazidMashrafi <shazid456@gmail.com>
2023-03-10 10:40:08 +0600

    initial commit

---
+++ Escape_sequence.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // \n and endl has the same use.
+    cout << "Hello\n" << "I am Shazid" <<endl<< "I am from Bangladesh";
+}
--- Hello_world.cpp
+++ Hello_world.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     cout<<"Hello world!\n";
-    cout<< "Hello" <<" world!";
+    cout<< "Hello" <<" world!"; //cout can print multiple outputs at the same time.
     return 0;
 }
