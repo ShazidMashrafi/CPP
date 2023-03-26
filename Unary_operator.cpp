c016e6f355cd8559a0efc3957342cbbf270ce282
ShazidMashrafi <shazid456@gmail.com>
2023-03-26 17:07:43 +0600

    initial commit

---
+++ Unary_operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=6,result;
+
+    result= +x; //Assigns + sign
+    cout<<result<<endl;
+
+    result=-x; //Assigns -sign
+    cout<<result<<endl;
+}
