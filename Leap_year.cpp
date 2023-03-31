5e57490f08e262c282d363fedc5a894843e4db2c
ShazidMashrafi <shazid456@gmail.com>
2023-03-31 12:24:30 +0600

    update logic

--- Leap_year.cpp
+++ Leap_year.cpp
@@ -7,7 +7,6 @@ int main()
     cout<<"Enter year: ";
     cin>>year;
 
-    if(year%4==0 && year%100!=0) cout<<"Leap year";
-    else if(year%400==0) cout<<"Leap year";
+    if((year%4==0 && year%100!=0) || year%400==0) cout<<"Leap year";
     else cout<<"Not leap year";
 }
