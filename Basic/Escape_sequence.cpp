e17ab0d116c8a5bb434a2d8bfc8a350c1291eb17
ShazidMahsrafi <shazid456@gmail.com>
2023-04-15 14:54:46 -0700

    check

--- Basic/Escape_sequence.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    // \n and endl has the same use.
-    cout << "Hello\n" << "I am Shazid" <<endl<< "I am from Bangladesh";
-}
--- Basic/Hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"Hello world!\n";
-    cout<< "Hello" <<" world!"; //cout can print multiple outputs at the same time.
-    return 0;
-}
