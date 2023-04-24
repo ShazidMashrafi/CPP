b7700fdc6a58991888d4f44d14cb9849ac2760f1
ShazidMahsrafi <shazid456@gmail.com>
2023-04-25 00:07:57 +0600

    initial commit

---
+++ Guessing_game.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        int guess,random;
+        cout<<"Guess a number between 1 and 5: ";
+        cin>>guess;
+        random=rand()%5+1;
+        if(random==guess)
+        {
+            cout<<"Congrats. You have guessed the correct number."<<endl<<endl;
+        }
+        else
+        {
+            cout<<"Sorry your guess is incorrect."<<endl<<"The number was "<<random<<"."<<endl<<"Try again."<<endl<<endl;
+        }
+        
+    }
+}
