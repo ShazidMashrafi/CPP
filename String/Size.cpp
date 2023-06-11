f5ecc15c95c3f310ef55a0bc2e77ec4b5d5e477b
ShazidMahsrafi <shazid456@gmail.com>
2023-06-11 15:06:02 +0600

    update

--- String/Size.cpp
+++ String/Size.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-    string s1="Shazid ";
+    string s1="Shazid";
     string s2="Mashrafi";
-    string s3;
 
-    s3=s1 + s2; //string concatenation
-    cout<<s3<<endl;
+    int len=s1.size();
+    cout<<len<<endl;
+
+    len=s2.size();
+    cout<<len<<endl;
 }
