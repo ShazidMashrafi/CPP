d2d47530153a030b0decc9738c4a60f94de65a7d
ShazidMahsrafi <shazid456@gmail.com>
2023-06-11 15:03:39 +0600

    initial commit

---
+++ String/Concatenation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s1="Shazid ";
+    string s2="Mashrafi";
+    string s3;
+
+    s3=s1 + s2; //string concatenation
+    cout<<s3<<endl;
+}
