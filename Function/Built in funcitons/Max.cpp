6b4a3d88dda1169209b59e298e6adabda51d597b
ShazidMahsrafi <shazid456@gmail.com>
2023-06-14 19:06:10 +0600

    initial commit

---
+++ Function/Built in funcitons/Max.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cin>>x>>y;
+    z=max(x,y);
+    cout<<"Maximum of "<<x<<" and "<<y<< " is "<<z<<endl;
+}
---
+++ Function/Built in funcitons/Min.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cin>>x>>y;
+    z=min(x,y);
+    cout<<"Minimum of "<<x<<" and "<<y<< " is "<<z<<endl;
+}
