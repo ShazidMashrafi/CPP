0c65f7a372734994d8140bc3666d41169a7a75f1
ShazidMahsrafi <shazid456@gmail.com>
2023-12-19 20:09:54 +0600

    updated files

---
+++ Bit manipulation/Tricks/Multiplied & Divided by power of 2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void print(int num)
+{
+    cout<<num<<": ";
+    for(int i=8; i>=0; --i)
+        cout<< ((num>>i) & 1); // prints binary;
+    cout<<endl;
+}
+
+int main()
+{
+    int n=10;
+    print(n);
+    int i=1;
+    cout<<"Multiply and divide by 2^"<<i<<": "<<endl;
+    print(n<<i);
+    print(n>>i);
+    i=2;
+    cout<<"Multiply and divide by 2^"<<i<<": "<<endl;
+    print(n<<i);
+    print(n>>i);
+}
