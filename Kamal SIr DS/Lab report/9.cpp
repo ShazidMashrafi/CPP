d91f9f02e2e349a7f39f51bd0a82505e69e477ee
ShazidMahsrafi <shazid456@gmail.com>
2024-03-31 16:53:09 +0600

    fixed code

--- Kamal SIr DS/Lab report/9.cpp
+++ Kamal SIr DS/Lab report/9.cpp
@@ -1,25 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void DELETE(char info[], int link[], int start)
+void DISPLAY(char info[], int link[], int start)
 {
     int ptr = start;
-    cout << "\nBefore deleting the last item: ";
-    while (ptr != -1)
-    {
-        cout << info[ptr];
-        ptr = link[ptr];
-    }
-
-    ptr = start;
-    while (link[link[ptr]] != -1)
-    {
-        ptr = link[ptr];
-    }
-    link[ptr]=-1;
-
-    ptr = start;
-    cout << "\nAfter deleting the first item: ";
     while (ptr != -1)
     {
         cout << info[ptr];
@@ -27,10 +11,40 @@ void DELETE(char info[], int link[], int start)
     }
 }
 
+void DELETE(char info[], int link[], int start)
+{
+    cout << "\nBefore deleting the first item: ";
+    DISPLAY(info, link, start);
+
+    char item;
+    cout << "\nEnter the item: ";
+    cin >> item;
+
+    if(item==info[start])
+        start=link[start];
+    else
+    {
+        int ptr = start, prevptr = start;
+        while (ptr != -1)
+        {
+            ptr = link[ptr];
+            if (item == info[ptr])
+            {
+                link[prevptr] = link[ptr];
+                break;
+            }
+            prevptr = ptr;
+        }
+    }
+    
+    cout << "\nAfter deleting the first item: ";
+    DISPLAY(info, link, start);
+}
+
 int main()
 {
     char info[13] = {"  OT  X NIE "};
     int link[13] = {-2, -2, 5, -1, -2, 10, 9, -2, 2, 3, 6, -2, -2};
     int start = 8;
-    DELETE(info,link,start);
+    DELETE(info, link, start);
 }
