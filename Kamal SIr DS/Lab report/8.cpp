2e44488f5e4356bf49a54d2a4ccb7d8e08162f8b
ShazidMahsrafi <shazid456@gmail.com>
2024-03-31 16:47:16 +0600

    added display function

--- Kamal SIr DS/Lab report/8.cpp
+++ Kamal SIr DS/Lab report/8.cpp
@@ -1,19 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void DELETE(char info[], int link[], int start)
+void DISPLAY(char info[], int link[], int start)
 {
     int ptr = start;
-    cout << "\nBefore deleting the first item: ";
-    while (ptr != -1)
-    {
-        cout << info[ptr];
-        ptr = link[ptr];
-    }
-
-    start = link[start];
-    ptr = start;
-    cout << "\nAfter deleting the first item: ";
     while (ptr != -1)
     {
         cout << info[ptr];
@@ -21,10 +11,21 @@ void DELETE(char info[], int link[], int start)
     }
 }
 
+void DELETE(char info[], int link[], int start)
+{
+    cout << "\nBefore deleting the first item: ";
+    DISPLAY(info, link, start);
+
+    start = link[start];
+
+    cout << "\nAfter deleting the first item: ";
+    DISPLAY(info, link, start);
+}
+
 int main()
 {
     char info[13] = {"  OT  X NIE "};
     int link[13] = {-2, -2, 5, -1, -2, 10, 9, -2, 2, 3, 6, -2, -2};
     int start = 8;
-    DELETE(info,link,start);
+    DELETE(info, link, start);
 }
