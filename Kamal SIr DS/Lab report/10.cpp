7225fd728ff1e97836e3570fd24db42e4387a7e7
ShazidMahsrafi <shazid456@gmail.com>
2024-03-31 16:53:42 +0600

    update spacing

--- Kamal SIr DS/Lab report/10.cpp
+++ Kamal SIr DS/Lab report/10.cpp
@@ -1,24 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+void FIND(char info[], int link[], int start)
+{
+    char key;
+    cout << "\nEnter the key item: ";
+    cin >> key;
+    int ptr = start;
+    while (ptr != -1)
+    {
+        if (info[ptr] == key)
+        {
+            cout << "\nFound at position: " << ptr + 1 << endl;
+            return;
+        }
+        ptr = link[ptr];
+    }
+    cout << "\nKey not found" << endl;
+}
 
 int main()
 {
-    int start,ptr;
-    char key;
-    char info[13]={"  OT  X NIE "};
-    int link[13]={-2,-2,5,-1,-2,10,9,-2,2,3,6,-2,-2};
-    start=8;
-    cout<<"\nEnter the key item: ";
-    cin>>key;
-    ptr=start;
-    while(ptr!=-1)
-    {
-        if(info[ptr]==key)
-        {
-            cout<<"\nFound at position: "<<ptr+1<<endl;
-            return 0;
-        }
-        ptr=link[ptr];
-    }
-    cout<<"\nKey not found"<<endl;
+    int start;
+    char info[13] = {"  OT  X NIE "};
+    int link[13] = {-2, -2, 5, -1, -2, 10, 9, -2, 2, 3, 6, -2, -2};
+    start = 8;
+    FIND(info, link, start);
 }
