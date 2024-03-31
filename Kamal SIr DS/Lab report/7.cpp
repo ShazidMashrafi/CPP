1ce7ac9b46cbcd0644b0b41004ec3b33e5af50b7
ShazidMahsrafi <shazid456@gmail.com>
2024-03-31 16:55:09 +0600

    added traverse function

--- Kamal SIr DS/Lab report/7.cpp
+++ Kamal SIr DS/Lab report/7.cpp
@@ -1,15 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+void TRAVERSE(char info[], int link[], int start)
 {
-    char info[13]={"  OT  X NIE "};
-    int link[13]={-2,-2,5,-1,-2,10,9,-2,2,3,6,-2,-2};
-    int start=8;
     int ptr=start;
     while(ptr!=-1)
     {
         cout<<info[ptr];
         ptr=link[ptr];
     }
+}
+
+int main()
+{
+    char info[13]={"  OT  X NIE "};
+    int link[13]={-2,-2,5,-1,-2,10,9,-2,2,3,6,-2,-2};
+    int start=8;
+    TRAVERSE(info,link,start);
 }
