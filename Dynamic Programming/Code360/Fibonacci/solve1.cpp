c9dc016806301938bf446eba8c5f71cb2d4f3215
ShazidMahsrafi <shazid456@gmail.com>
2024-04-25 01:38:03 +0600

    added

--- Dynamic Programming/Code360/Fibonacci/solve1.cpp
+++ Dynamic Programming/Code360/Fibonacci/solve1.cpp
@@ -1,17 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll 	long long
+
 const int N = 1e5+10;
-
-// 0 1 1 2 3 5 8 .....
-
 vector<int>dp(N,-1);
 
 int fib(int n)
 {
     if(n==0)    return 0;
     if(n==1)    return 1;
-    if(dp[n] != -1)     return dp[n];   // Memoise technic
+    if(dp[n] != -1)     return dp[n];
     dp[n] = fib(n-1) + fib(n-2);
     return dp[n];
 }
@@ -22,4 +19,3 @@ int main()
     cin>>n;
     cout<<fib(n)<<endl;
 }
-// Time complexity -> O(n)
--- Dynamic Programming/Code360/Fibonacci/solve2.cpp
+++ Dynamic Programming/Code360/Fibonacci/solve2.cpp
@@ -1,21 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll 	long long
-const int N = 1e5+10;
-
-// 0 1 1 2 3 5 8 .....
-
-vector<int>dp(N,-1);
-
-
 int main()
 {
     int n;
     cin>>n;
+    vector<int>dp(n+1);
     dp[0]=0;
-    dp[1] = 1;
+    dp[1]=1;
     for(int i=2; i<=n; ++i)
         dp[i]=dp[i-1]+dp[i-2];
     cout<<dp[n]<<endl;
 }
-// Time complexity -> O(n)
